8444248d92184eb03fed0081b95097f99350c79e
Ramaekers,Aldrik A.N <a.ramaekers@student.fontys.nl>
2020-11-04 12:55:57 +0100

    comments

--- lamp/lamp.ino
+++ lamp/lamp.ino
@@ -1,12 +1,9 @@
 
 #include <Charliplexing.h>
-#include <BLEduino.h>
 #include "Myfont.h"
 
-
-BLEduino BLE;
-
 void displayText(char* txt);
+void turnOnAllLeds();
 
 void setup()
 {
@@ -21,21 +18,26 @@ void setup()
   delay(300);
   displayText("...");
   delay(300);
+
+  turnOnAllLeds();
 }
 
 int strLen = 0;
 #define strMaxLen 10
-char str[strMaxLen];
+char str[strMaxLen]; // buffer for storing input from user
 
-int brightness = 127;
-bool manualBrightness = false;
-bool isFlickering = false;
-bool showingNewValue = false;
-long long newValueStamp = 0;
+int brightness = 127; // current lamp brightness (0 - 127)
+bool manualBrightness = false; // is the brightness being manually controlled
+
+bool isFlickering = false; // is the lamp flickering
 long long flickerStamp = 0;
 bool flickerOn = true;
+
+bool showingNewValue = false; // showing new brightness in text
+long long newValueStamp = 0;
 char newValueStr[strMaxLen];
 
+// display text on the lamp. 3 characters max.
 void displayText(char* txt) {
   int len = strlen(txt);
   if (len > 3) len = 3;
@@ -55,11 +57,13 @@ int fast_atoi(const char * str)
     return val;
 }
 
+// keep brightness between 0 and 127.
 inline void keepBrightnessBetweenBounds() {
   if (brightness > 127) brightness = 127;
   if (brightness < 0) brightness = 0;
 }
 
+// handle user input for patterns.
 void handlePatternChange() {
   if (str[0] != '!' && str[0] != '#') return;
 
@@ -73,6 +77,7 @@ void handlePatternChange() {
   }
 }
 
+// handle user input for manual brightness changes.
 void handleBrightnessChange() {
   if (str[0] == '!' || str[0] == '#') return;
   
@@ -89,20 +94,31 @@ void handleBrightnessChange() {
     newValueStamp = millis();
     showingNewValue = true;
     
-    int percentage = map(brightness, 0, 127, 0, 100);
+    int percentage = map(brightness, 0, 127, 0, 100); // calculate percentage to show on lamp.
     sprintf(newValueStr, "%d", percentage);
     LedSign::Clear();
+
+    LedSign::SetBrightness(brightness);
   }
 }
 
+// Turn on all leds.
+void turnOnAllLeds() {
+  for (int row = 0; row < DISPLAY_ROWS; row++)
+    for (int col = 0; col < DISPLAY_COLS; col++)
+      LedSign::Set(col, row, 1);
+}
+
 void loop()
-{      
+{ 
+  // if the brightness is not manually controlled, calculate brightness based on input from LDR.
   if (!manualBrightness) {
     int ldrVal = analogRead(A3);
     brightness = map(ldrVal, 100, 1000, 127, 0);
     keepBrightnessBetweenBounds();
   }
-  
+
+  // handle data retrieved from user.
   if (Serial.available() > 0) {
     char ch = Serial.read();
   
@@ -118,27 +134,25 @@ void loop()
     }
   }
 
+  // show text on screen if value recently changed.
   if (showingNewValue) { 
     displayText(newValueStr);
     if (millis() - newValueStamp > 1000) {
       showingNewValue = false;
+      turnOnAllLeds();
     }
-  } else {
-    if (isFlickering) {
-      if (millis() - flickerStamp < 300) {
-        LedSign::SetBrightness(flickerOn ? brightness : 0);        
-      }
-      else {
-        flickerStamp = millis();
-        flickerOn = !flickerOn;
-      }       
+  }
+
+  // either flicker or display a constant brightness.
+  if (isFlickering) {
+    if (millis() - flickerStamp < 300) {
+      LedSign::SetBrightness(flickerOn ? brightness : 0);        
     }
     else {
-      LedSign::SetBrightness(brightness);
-    }
-    
-    for (int row = 0; row < DISPLAY_ROWS; row++)
-        for (int col = 0; col < DISPLAY_COLS; col++)
-          LedSign::Set(col, row, 1);
+      flickerStamp = millis();
+      flickerOn = !flickerOn;
+    }       
+  } else {
+    LedSign::SetBrightness(brightness);
   }
 }
