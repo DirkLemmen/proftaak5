08676a3493b46a90d93e26749ff759d9c3b002a4
Ramaekers,Aldrik A.N <a.ramaekers@student.fontys.nl>
2020-11-04 11:43:11 +0100

    poep

--- lamp/lamp.ino
+++ lamp/lamp.ino
@@ -6,7 +6,7 @@
 
 BLEduino BLE;
 
-void displayNewBrightness(String value);
+void displayText(char* txt);
 
 void setup()
 {
@@ -15,10 +15,12 @@ void setup()
   pinMode(A3, INPUT);
   Serial.begin(9600);
 
-  BLE.begin(); //Initialize BLE object
-  BLE.sendCommand(COMMAND_RESET); //Start advertising BLEduino
-
-  displayNewBrightness("100");
+  displayText(".");
+  delay(300);
+  displayText("..");
+  delay(300);
+  displayText("...");
+  delay(300);
 }
 
 int strLen = 0;
@@ -27,15 +29,17 @@ char str[strMaxLen];
 
 int brightness = 127;
 bool manualValue = false;
+bool showingNewValue = false;
+long long newValueStamp = 0;
+char newValueStr[strMaxLen];
 
-void displayNewBrightness(String value) {
-  unsigned char* txt = (unsigned char*)(value.c_str());
-  int len = value.length();
-  int xoff=0;
+void displayText(char* txt) {
+  int len = strlen(txt);
+  if (len > 3) len = 3;
+  int xoff=3-len;
   for(int j=0; j<len; j++){
       Myfont::Draw(xoff + j*4, txt[j]);
   }
-  delay(3000);
 }
 
 // https://stackoverflow.com/questions/16826422/c-most-efficient-way-to-convert-string-to-int-faster-than-atoi
@@ -48,58 +52,46 @@ int fast_atoi(const char * str)
     return val;
 }
 
+inline void keepBrightnessBetweenBounds() {
+  if (brightness > 127) brightness = 127;
+  if (brightness < 0) brightness = 0;
+}
+
+void handleBrightnessChange() {
+  manualValue = true;
+      int newBrightness = fast_atoi(str);
+
+      if (strcmp(str, "-1") == 0) {
+        manualValue = false;
+      }
+      else {
+        brightness = newBrightness;
+        keepBrightnessBetweenBounds();
+
+        newValueStamp = millis();
+        showingNewValue = true;
+
+        int percentage = map(brightness, 0, 127, 0, 100);
+        //sprintf(newValueStr, "%d", percentage);
+        sprintf(newValueStr, "%s", str);
+        LedSign::Clear();
+      }
+
+}
+
 void loop()
-{ 
-   if(BLE.available(FIRMATA_READ)){
-    
-        //Read packet
-        //The parameter is also optional here.  
-        //BLE.read() will return the last packet received, regardless of pipe.
-        BLEPacket packet = BLE.read(FIRMATA_READ);
-
-        //Parse packet
-        //uint8_t length = packet.length; //not used here
-        uint8_t * data = packet.data;
-        /************************************
-        Packet Structure for LED data
-        ||    0    |     1    |      2    ||
-        || LED Pin | Not Used | Pin State ||
-        Using firmata pipe
-        *************************************/
-
-        //Parse LED data
-        byte led_pin = data[0];
-        byte led_state = data[2];
-
-        pinMode(led_pin, OUTPUT);
-        digitalWrite(led_pin, led_state);
-
-    }
-    
+{      
   if (!manualValue) {
     int ldrVal = analogRead(A3);
     brightness = map(ldrVal, 100, 1000, 127, 0);
-    if (brightness > 127) brightness = 127;
-    if (brightness < 0) brightness = 0;
+    keepBrightnessBetweenBounds();
   }
   
   if (Serial.available() > 0) {
     char ch = Serial.read();
   
     if (ch == '\n') {
-      manualValue = true;
-      int newBrightness = fast_atoi(str);
-      if (newBrightness == -1) {
-        manualValue = false;  
-      }
-      else {
-        if (newBrightness > 127) newBrightness = 127;
-        if (newBrightness < 0) newBrightness = 0;
       
-        brightness = newBrightness;
-        //displayNewBrightness(str);
-      }
-
       memset(str, 0, strMaxLen);
       strLen = 0;
     }
@@ -109,8 +101,15 @@ void loop()
   }
   
   LedSign::SetBrightness(brightness);
-  
-  for (int row = 0; row < DISPLAY_ROWS; row++)
-    for (int col = 0; col < DISPLAY_COLS; col++)
-      LedSign::Set(col, row, 1);
+
+  if (showingNewValue) { 
+    displayText(newValueStr);
+    if (millis() - newValueStamp > 1000) {
+      showingNewValue = false;
+    }
+  } else {
+    for (int row = 0; row < DISPLAY_ROWS; row++)
+      for (int col = 0; col < DISPLAY_COLS; col++)
+        LedSign::Set(col, row, 1);
+  }
 }
